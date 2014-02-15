dfe62f8f1d60f44ab6f78a90f15289c4128a984a
Johann Weiser <johann.weiser@aon.at>
2014-02-15 18:01:00 +0100

    Comments added.

--- src/weiser/HelloTeam.java
+++ src/weiser/HelloTeam.java
@@ -1,11 +1,20 @@
 package weiser;
 
+/**
+ * Testing the team functions of git and github.
+ * @author Johann Weiser
+ *	@version 2014-02-15
+ */
 public class HelloTeam {
 	
 	public static void helloFromWeiser1() {
 		System.out.println("Hello to all of you from \"weiser1\"!");
 	}
 
+	/**
+	 * The main method in order to start the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		helloFromWeiser1();
 	}
