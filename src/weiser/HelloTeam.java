29b21a27bc65549b5ba6c914d765aeb76fe08607
Johann Weiser1 <johann.weiser@aon.at>
2014-02-15 19:12:00 +0100

    some comments

--- src/weiser/HelloTeam.java
+++ src/weiser/HelloTeam.java
@@ -3,10 +3,14 @@ package weiser;
 /**
  * Testing the team functions of git and github.
  * @author Johann Weiser
- *	@version 2014-02-15
+ * @author Clone of Johann Weiser
+ * @version 2014-02-15
  */
 public class HelloTeam {
 	
+	/**
+	 * Greetings from the clone. 
+	 */
 	public static void helloFromWeisert1() {
 		System.out.println("An alle liebe Grüße von \"weisert1\"!!!!!!");
 	}
