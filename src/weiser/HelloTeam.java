27c36b61ad8062b1bd92dbbb531ddb52a7552d5b
Johann Weiser1 <johann.weiser@aon.at>
2014-02-15 18:10:49 +0100

    hkjhk

--- src/weiser/HelloTeam.java
+++ src/weiser/HelloTeam.java
@@ -2,12 +2,16 @@ package weiser;
 
 public class HelloTeam {
 	
+	public static void helloFromWeisert1() {
+		System.out.println("An alle liebe Grüße von \"weisert1\"!!!!!!");
+	}
 	public static void helloFromWeiser1() {
 		System.out.println("Hello to all of you from \"weiser1\"!");
 	}
 
 	public static void main(String[] args) {
 		helloFromWeiser1();
+		helloFromWeisert1();
 	}
 
 }
