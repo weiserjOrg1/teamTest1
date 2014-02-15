e407586962953f4412d05ecfce56e26bc728286f
Johann Weiser <johann.weiser@aon.at>
2014-02-15 16:55:48 +0100

    First version of project

---
+++ src/weiser/HelloTeam.java
@@ -0,0 +1,13 @@
+package weiser;
+
+public class HelloTeam {
+	
+	public static void helloFromWeiser1() {
+		System.out.println("Hello to all of you from \"weiser1\"!");
+	}
+
+	public static void main(String[] args) {
+		helloFromWeiser1();
+	}
+
+}
